486ba23ddf56bc5141f7581083148f02b22a4a5d
Krisha Parmar <parmarkrisha10@gmail.com>
2025-09-07 12:04:33 +0530

    Update adv_app.py

--- adv_app.py
+++ adv_app.py
@@ -8,19 +8,20 @@ model = pickle.load(open('linear_reg.sav', 'rb'))
 st.title('Hospital Total Cost Prediction App')
 
 # Input features
-Age = st.number_input('Age', min_value=0.0)
-Body_Weight = st.number_input('Body_Weight', min_value=0.0)
-Body_Height = st.number_input('Body_Height', min_value=0.0)
-BP_High = st.number_input('BP_High', min_value=0.0)
-BP_Low = st.number_input('BP_Low', min_value=0.0)
-HB = st.number_input('HB', min_value=0.0)
-UREA = st.number_input('UREA', min_value=0.0)
-Creatine = st.number_input('Creatine', min_value=0.0)
+TOTAL_COST_TO_HOSPITAL = st.number_input('TOTAL_COST_TO_HOSPITAL', min_value=0.0)
+TOTAL_AMOUNT_BILLED_TO_THE_PATIENT = st.number_input('TOTAL_AMOUNT_BILLED_TO_THE_PATIENT', min_value=0.0)
+CONCESSION = st.number_input('CONCESSION', min_value=0.0)
+ACTUAL_RECEIVABLE_AMOUNT = st.number_input('ACTUAL_RECEIVABLE_AMOUNT', min_value=0.0)
+TOTAL_LENGTH_OF_STAY = st.number_input('TOTAL_LENGTH_OF_STAY', min_value=0.0)
+LENGTH_OF_STAY_ICU = st.number_input('LENGTH_OF_STAY_ICU', min_value=0.0)
+LENGTH_OF_STAY_WARD = st.number_input('LENGTH_OF_STAY_WARD', min_value=0.0)
+COST_OF_IMPLANT = st.number_input('COST_OF_IMPLANT', min_value=0.0)
 
 # Make prediction
 if st.button('Predict Hospital Total Cost'):
-    input_data = np.array([[Age, Body_Weight, Body_Height, BP_High, BP_Low, HB, UREA, Creatine]])
+    input_data = np.array([[TOTAL_COST_TO_HOSPITAL, TOTAL_AMOUNT_BILLED_TO_THE_PATIENT, CONCESSION, ACTUAL_RECEIVABLE_AMOUNT, TOTAL_LENGTH_OF_STAY, LENGTH_OF_STAY_ICU, LENGTH_OF_STAY_WARD, COST_OF_IMPLANT]])
     prediction = model.predict(input_data)[0]
     st.success(f'Predicted Hospital Cost: {prediction:.2f}')
+
 
 
